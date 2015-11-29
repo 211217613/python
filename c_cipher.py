3f394b861b49a43f633c0a0fc6edbf4c1c6ca896
211217613 <rubenavazquez@gmail.com>
2015-11-29 00:21:46 -0800

    modified from hackingciphers text

--- c_cipher.py
+++ c_cipher.py
@@ -3,7 +3,7 @@ print message
 
 key = input('Input the key: ')
 mode = 'encrypt'
-LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LETTERS = "0123456789BCDEFGHIJKLMNOPQRSTUVWXYZ"
 cipherText = ''
 message = message.upper()
 counter = 0
@@ -16,12 +16,16 @@ for symbol in message:
 		# print "num: " , num
 		if mode == 'encrypt':
 			num = num + key
-		if mode == 'decrypt':
+		# if mode == 'decrypt':
+		else:
 			num = num - key
+
+		#Handles overflow/underflow	
 		if num >= len(LETTERS):
 			num = num - len(LETTERS)
-		elif num < 0 :
-			num = num + len(LETTERS)
+		else:
+			if num < 0 :
+				num = num + len(LETTERS)
 		cipherText = cipherText + LETTERS[num]
 
 	else:
