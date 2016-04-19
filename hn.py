b507ab4daa964715c9fa4cdebff1ee7873f1b972
211217613 <rubenavazquez@gmail.com>
2016-04-18 21:55:18 -0700

    Added logging and config for logging

--- hn.py
+++ hn.py
@@ -2,34 +2,48 @@
 
 #Make sure to run this using python3
 import urllib.request
-import logging 
+import logging #https://docs.python.org/3.4/howto/logging.html#logging-basic-tutorial
 
 top_story_ids = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
 item_ids 	  = 'https://hacker-news.firebaseio.com/v0/item.json?print=pretty'
 job_ids 	  = 'https://hacker-news.firebaseio.com/v0/jobstories.json?print=pretty'
-job1 		  = 'https://hacker-news.firebaseio.com/v0/item/11500588.json?print=pretty'
+jobs = list()
 
+string = 'https://hacker-news.firebaseio.com/v0/item/{0!s}.json?print=pretty' 
 
 def setup_logging():
-	logging.basicConfig(filename='hn.log', level=logging.INFO)
+	logging.basicConfig(filename='hn.log', format='%(asctime)s %(message)s', datefmt='%I:%M:%S',level=logging.INFO, filemode='w')
 	logger = logging.getLogger(__name__)
 	logger.info('Starting logging')
+
+def string_formatting(html):
+	#html is a string, need to strip and only leave spaces between
+	logging.info("Removing commas... ")
+	html = html.replace(',','')
+	logging.info("html .... %s \n" % html )
+
+	logging.info("Splitting html into space delimited sections...")
+	ids = html.split()
+	logging.info("Now html is a list with ids ")
+	logging.info(ids)
+
+	string = 'https://hacker-news.firebaseio.com/v0/item/{}.json?print=pretty'.format(html[::])
+	jobs.append(string)
+	logging.info("jobs list contains %s" % jobs)
 
 def main():
 	setup_logging()
 	
-	with urllib.request.urlopen(job1) as response:
+	with urllib.request.urlopen(job_ids) as response:
 		html = response.read()
 	# Decode b' to ascii
-	html = html.decode("ascii")
-	length = len(html)
-	print(length)
-	e1 = top_story_ids[0]
-	# for element in e1:
-	# 	print(element)
-	print(html)
-	# if 'meta' in html:
-	# 	print("sdf")
+	html = html.decode("ascii")			# this converts html into a string NOT a list
+	logging.info("html before rstrip %s \n" % html)
+	html = html[2 : -3] 				# This gives a string with the beginning and end stripped
+	#
+	logging.info("logging" )
+	print("len %d " % len(html))
+	string_formatting(html)
 
 
 if __name__ == '__main__':
