6300d27b277c07ed7d7794ba6b46354cbaff7215
211217613 <rubenavazquez@gmail.com>
2016-04-19 10:23:41 -0700

    Add reference website and delete rsa1.py

--- rsa.py
+++ rsa.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python
-
+import gmpy2
 import os
-print "python"
 
 def gen_rsa_parameters():
-	r = os.urandom(63)  //returns a string 
-	print r
-	print "pythonnn"
+	r = os.urandom(63)  #returns a string 
+	print (r)
+	print ("pythonnn")
 
 	e = int(r.encode('hex'), 16)
+
+def main():
+	gen_rsa_parameters()
+
+if __name__ == '__main__':
+	main()
