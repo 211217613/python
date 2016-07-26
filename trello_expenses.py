8502f55bbacfa79823953057d97ba4237a8adce1
211217613 <rubenavazquez@gmail.com>
2016-07-26 16:54:06 -0500

    Remove old scraping code -> using py-trello

--- trello_expenses.py
+++ trello_expenses.py
@@ -3,9 +3,7 @@ import json
 # import urllib.request
 import sys
 from trello import TrelloClient
-# import trello
 
-# from urllib.request import HTTPError
 
 TRELLO_EXPIRATION = 'never'
 NAME 	= "name"
@@ -21,40 +19,13 @@ token = '0a46c305b380455a83176624e3e980fa8cfcba3b189a668558f3b03dc729a60e' # TRE
 oauth_secret = '47043b5293f8093f7121e233a080595855967e7cc145c82bd75f4817d6c7990b'
 
 client = TrelloClient(api_key=api_key, api_secret=api_secret, token=token , token_secret=oauth_secret )
+# Test board: https://trello.com/b/So69sspg/test-expenses
 
 # Getting boards
 board = client.list_boards()
 board_info = client.info_for_all_boards(all)
-board_expenses = client.get_board('BE89pW61')
-board_expenses.get_list('Comidas')
+board_expenses = client.get_board('So69sspg')
+# board_expenses.get_list('Comidas')
 
 for boards in board:
 	print boards
-
-
-
-
-# url = 'https://trello.com/b/BE89pW61/expenses.json'
-# url = 'https://trello.com/b/BE89pW61/expenses.json'
-# try:
-# 	url = 'https://trello.com/b/BE89pW61.json'
-# 	respone = urllib.request.urlopen(url)
-# except:
-# 	e = sys.exc_info()[0]
-# 	print(e)
-
-# with open('./expenses.json', 'r') as json_file:
-	# trello_board = json.load(json_file)
-# print( "Board: {}".format(trello_board[NAME]) )
-
-# for trello_list in trello_board[LISTS]:
-# 	if not trello_list [CLOSED]:
-# 		print("Lists: " + trello_list [NAME])
-# 		for trello_card in trello_board[CARD]:
-# 			print("Cards: " + trello_card[NAME])
-
-
-
-# response = urllib.request.urlopen(url)
-# html = response.read()
-# html = html.decode('')
