00e9b30660ad9547bd13d598f47454c642e3bf31
211217613 <rubenavazquez@gmail.com>
2016-07-26 18:16:55 -0500

    Clean up code a bit

--- trello_expenses.py
+++ trello_expenses.py
@@ -4,12 +4,6 @@ import json
 import sys
 from trello import TrelloClient
 
-
-TRELLO_EXPIRATION = 'never'
-NAME 	= "name"
-LISTS 	= "lists"
-CLOSED 	= "closed" 
-CARD 	= "cards"
 # Developer API key
 api_key = '2819ec494f41829d45bdea15e3cf20e0' # TRELLO_API_KEY
 api_secret = '80cc221711c53bc6c3fab2776079e4c7' 
@@ -18,14 +12,29 @@ api_secret = '80cc221711c53bc6c3fab2776079e4c7'
 token = '0a46c305b380455a83176624e3e980fa8cfcba3b189a668558f3b03dc729a60e' # TRELLO_TOKEN: OAUTH TOKEN
 oauth_secret = '47043b5293f8093f7121e233a080595855967e7cc145c82bd75f4817d6c7990b'
 
-client = TrelloClient(api_key=api_key, api_secret=api_secret, token=token , token_secret=oauth_secret )
 # Test board: https://trello.com/b/So69sspg/test-expenses
 
 # Getting boards
-board = client.list_boards()
-board_info = client.info_for_all_boards(all)
-board_expenses = client.get_board('So69sspg')
+# Returns a non-itereable board object
+
+
+# Returns a non-iterable card object that has a .from_json function
+# print (type(cards))
+
 # board_expenses.get_list('Comidas')
 
-for boards in board:
-	print boards
+def main():
+    client = TrelloClient(api_key=api_key, api_secret=api_secret, token=token , token_secret=oauth_secret )
+    board_expenses = client.get_board('So69sspg') # test board
+    # board_expenses_list = board_expenses.all_lists()
+    board_expenses.add_list("test list")
+    cards =  board_expenses.get_cards()
+
+    for card in cards:
+        print(card)
+
+    
+
+if __name__ == '__main__':
+    main()
+
