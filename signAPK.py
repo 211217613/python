50efbb08bfb62e2a2f7069dfd86b3635fd7df153
211217613 <rubenavazquez@gmail.com>
2016-08-06 23:04:43 -0500

    Pep8 it

--- signAPK.py
+++ signAPK.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3.4m
 
-#Specify what apk to sign
-#Get input from user or it can just sign all apks/
-#Note paths must be in double quotes
+# Specify what apk to sign
+# Get input from user or it can just sign all apks/
+# Note paths must be in double quotes
+
 
 from subprocess import call
 import sys
 # TODO: fix path to use $USER env_var
 def main():
 
-	#get path to apk
+	# get path to apk
 	# apkpath1 = input("absolute path to apk\n")
 	# apkpath = "/home/ruben/Dev/Cydia/VioletCydia/VioletCydia.apk"
 	apkpath = sys.argv[1] 
@@ -32,5 +33,5 @@ def main():
 if __name__ == '__main__':
 	main()
 
-#jarsigner -verbose -keystore ~/.android/debug.keystore -storepass android -keypass android 
-#jarsigner -verbose -keystore ~/.android/debug.keystore -storepass android -keypass android /path/to/MY.apk androiddebugkey
+# jarsigner -verbose -keystore ~/.android/debug.keystore -storepass android -keypass android 
+# jarsigner -verbose -keystore ~/.android/debug.keystore -storepass android -keypass android /path/to/MY.apk androiddebugkey
